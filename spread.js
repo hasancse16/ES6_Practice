1f89d7670240deaa5b83a8cac5b738b77ae278ac
Md Hasanur Rahman <85791830+hasancse16@users.noreply.github.com>
2022-07-26 16:19:25 +0600

    Spread

---
+++ spread.js
@@ -0,0 +1,11 @@
+
+
+let poorCountry = ['Bangladesh', 'srilanka'];
+
+let richCountry = ['India', 'China', 'Japan'];
+
+// let richCountrys = [...poorCountry, 'India', 'China', 'Japan'];
+
+richCountry.push(poorCountry);
+
+console.log(richCountry)
